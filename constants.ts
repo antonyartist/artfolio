f434582a02c45e504fd856e823870b0342feee59
antonyartist <antonyartist100@gmail.com>
2025-11-30 00:16:02 +0530

    changes add

--- constants.ts
+++ constants.ts
@@ -22,183 +22,183 @@ export const ACHIEVEMENTS = [
     year: '1995',
     title: 'Kerala Lalithakala Akademi - Highly Commended Certificate',
     description: 'For a painting at 24th State Exhibition of Art - 1995.',
-    image: '/assets/certificate/lalithakala.jpeg',
+    image: '/artfolio/assets/certificate/lalithakala.jpeg',
   },
 ];
 
 export const HERO_BACKGROUND_IMAGES = [
-  '/assets/backgroundimage/ar1.jpeg',
-  '/assets/backgroundimage/ar2.jpeg',
-  '/assets/backgroundimage/ar6.jpeg',
-  '/assets/backgroundimage/ar3.jpeg',
-  '/assets/backgroundimage/ar4.jpeg',
+  '/artfolio/assets/backgroundimage/ar1.jpeg',
+  '/artfolio/assets/backgroundimage/ar2.jpeg',
+  '/artfolio/assets/backgroundimage/ar6.jpeg',
+  '/artfolio/assets/backgroundimage/ar3.jpeg',
+  '/artfolio/assets/backgroundimage/ar4.jpeg',
 
 ];
 
 export const NEWS_ARTICLE_IMAGES = [
   {
-    src: '/assets/newscut/news1.jpeg',
+    src: '/artfolio/assets/newscut/news1.jpeg',
   },
   {
-    src: '/assets/newscut/news7.jpeg',
+    src: '/artfolio/assets/newscut/news7.jpeg',
   },
   {
-    src: '/assets/newscut/news6.jpeg',  
+    src: '/artfolio/assets/newscut/news6.jpeg',  
   },
   {
-    src: '/assets/newscut/news5.jpeg',
+    src: '/artfolio/assets/newscut/news5.jpeg',
   },
 ];
 
 export const ALTAR_ART = [
   {
-    src: '/assets/altar/altarrr.jpeg',
+    src: '/artfolio/assets/altar/altarrr.jpeg',
   },
 ];
 
 export const WALL_ART = [
   {
-    src: '/assets/wallartconf/wall1.jpeg',
+    src: '/artfolio/assets/wallartconf/wall1.jpeg',
   },
   {
-    src: '/assets/wallartconf/wall3.jpeg',
+    src: '/artfolio/assets/wallartconf/wall3.jpeg',
   },
   {
-    src: '/assets/wallartconf/wall2.jpeg',
+    src: '/artfolio/assets/wallartconf/wall2.jpeg',
   },
 ];
 export const GALLERY_IMAGES = [
   // Collection 1
-  '/assets/artreal/art1.jpeg',
-  '/assets/artreal/art2.jpeg',
-  '/assets/artreal/art3.jpeg',
+  '/artfolio/assets/artreal/art1.jpeg',
+  '/artfolio/assets/artreal/art2.jpeg',
+  '/artfolio/assets/artreal/art3.jpeg',
   
   
   // Collection 2
-  '/assets/artreal/art4.jpeg',
-  '/assets/artreal/art5.jpeg',
-  '/assets/artreal/art6.jpeg',
+  '/artfolio/assets/artreal/art4.jpeg',
+  '/artfolio/assets/artreal/art5.jpeg',
+  '/artfolio/assets/artreal/art6.jpeg',
   
   // Collection 3
-  '/assets/artreal/art7.jpeg',
-  '/assets/artreal/art8.jpeg',
-  '/assets/artreal/art9.jpeg',
+  '/artfolio/assets/artreal/art7.jpeg',
+  '/artfolio/assets/artreal/art8.jpeg',
+  '/artfolio/assets/artreal/art9.jpeg',
   
   // Collection 4
-  '/assets/artreal/art10.jpeg',
-  '/assets/artreal/art11.jpeg',
-  '/assets/artreal/art12.jpeg',
+  '/artfolio/assets/artreal/art10.jpeg',
+  '/artfolio/assets/artreal/art11.jpeg',
+  '/artfolio/assets/artreal/art12.jpeg',
   
   // Collection 5
-  '/assets/artreal/art13.jpeg',
-  '/assets/artreal/art14.jpeg',
-  '/assets/artreal/art15.jpeg',
+  '/artfolio/assets/artreal/art13.jpeg',
+  '/artfolio/assets/artreal/art14.jpeg',
+  '/artfolio/assets/artreal/art15.jpeg',
 
   // Collection 6
-  '/assets/artreal/art16.jpeg',
-  '/assets/artreal/art17.jpeg',
-  '/assets/artreal/art18.jpeg',
+  '/artfolio/assets/artreal/art16.jpeg',
+  '/artfolio/assets/artreal/art17.jpeg',
+  '/artfolio/assets/artreal/art18.jpeg',
   
  // Collection 7
-  '/assets/artreal/art19.jpeg',
-  '/assets/artreal/art20.jpeg',
-  '/assets/artreal/art21.jpeg',
+  '/artfolio/assets/artreal/art19.jpeg',
+  '/artfolio/assets/artreal/art20.jpeg',
+  '/artfolio/assets/artreal/art21.jpeg',
 ];
 
 export const ART_GALLERY = [
   // Collection 1
-  '/assets/artreal/art1.jpeg',
-  '/assets/artreal/art2.jpeg',
-  '/assets/artreal/art3.jpeg',
+  '/artfolio/assets/artreal/art1.jpeg',
+  '/artfolio/assets/artreal/art2.jpeg',
+  '/artfolio/assets/artreal/art3.jpeg',
   
   
   // Collection 2
-  '/assets/artreal/art4.jpeg',
-  '/assets/artreal/art5.jpeg',
-  '/assets/artreal/artgal6.jpeg',
+  '/artfolio/assets/artreal/art4.jpeg',
+  '/artfolio/assets/artreal/art5.jpeg',
+  '/artfolio/assets/artreal/artgal6.jpeg',
   
   // Collection 3
-  '/assets/artreal/artgal3.jpeg',
-  '/assets/artreal/artgal2.jpeg',
-  '/assets/artreal/artgal4.jpeg',
+  '/artfolio/assets/artreal/artgal3.jpeg',
+  '/artfolio/assets/artreal/artgal2.jpeg',
+  '/artfolio/assets/artreal/artgal4.jpeg',
   
   // Collection 4
-  '/assets/artreal/artgal5.jpeg',
-  '/assets/artreal/art11.jpeg',
-  '/assets/artreal/art12.jpeg',
+  '/artfolio/assets/artreal/artgal5.jpeg',
+  '/artfolio/assets/artreal/art11.jpeg',
+  '/artfolio/assets/artreal/art12.jpeg',
   
   // Collection 5
-  '/assets/artreal/art13.jpeg',
-  '/assets/artreal/art14.jpeg',
-  '/assets/artreal/art15.jpeg',
+  '/artfolio/assets/artreal/art13.jpeg',
+  '/artfolio/assets/artreal/art14.jpeg',
+  '/artfolio/assets/artreal/art15.jpeg',
 
   // Collection 6
-  '/assets/artreal/art16.jpeg',
-  '/assets/artreal/art17.jpeg',
-  '/assets/artreal/art18.jpeg',
+  '/artfolio/assets/artreal/art16.jpeg',
+  '/artfolio/assets/artreal/art17.jpeg',
+  '/artfolio/assets/artreal/art18.jpeg',
   
  // Collection 7
-  '/assets/artreal/art19.jpeg',
-  '/assets/artreal/art20.jpeg',
-  '/assets/artreal/art21.jpeg',
+  '/artfolio/assets/artreal/art19.jpeg',
+  '/artfolio/assets/artreal/art20.jpeg',
+  '/artfolio/assets/artreal/art21.jpeg',
 ];
 
 export const CONFIDENT_IMAGES = [
   // Collection 1
-  '/assets/confidentwall/conf4.jpeg',
-  '/assets/artreal/art2.jpeg',
-  '/assets/confidentwall/conf1.jpeg',
+  '/artfolio/assets/confidentwall/conf4.jpeg',
+  '/artfolio/assets/artreal/art2.jpeg',
+  '/artfolio/assets/confidentwall/conf1.jpeg',
   
   
   // Collection 2
-   '/assets/artreal/art6.jpeg',
-  '/assets/confidentwall/conf2.jpeg',
-  '/assets/confidentwall/conf5.jpeg',
+   '/artfolio/assets/artreal/art6.jpeg',
+  '/artfolio/assets/confidentwall/conf2.jpeg',
+  '/artfolio/assets/confidentwall/conf5.jpeg',
   
   // Collection 3
-  '/assets/confidentwall/conf3.jpeg',
-  '/assets/confidentwall/conf8.jpeg',
-  '/assets/confidentwall/conf9.jpeg',
+  '/artfolio/assets/confidentwall/conf3.jpeg',
+  '/artfolio/assets/confidentwall/conf8.jpeg',
+  '/artfolio/assets/confidentwall/conf9.jpeg',
 
 ];
 
 export const CREATIVE_ART = [
   // Collection 1
-  '/assets/creativeartworks/artcret1.jpeg',
-  '/assets/creativeartworks/artcret2.jpeg',
-  '/assets/creativeartworks/artcret3.jpeg',
+  '/artfolio/assets/creativeartworks/artcret1.jpeg',
+  '/artfolio/assets/creativeartworks/artcret2.jpeg',
+  '/artfolio/assets/creativeartworks/artcret3.jpeg',
   
   
   // Collection 2
-  '/assets/creativeartworks/artcret4.jpeg',
-  '/assets/creativeartworks/artcret5.jpeg',
-  '/assets/creativeartworks/artcret6.jpeg',
+  '/artfolio/assets/creativeartworks/artcret4.jpeg',
+  '/artfolio/assets/creativeartworks/artcret5.jpeg',
+  '/artfolio/assets/creativeartworks/artcret6.jpeg',
   
   // Collection 3
-  '/assets/creativeartworks/artcret7.jpeg',
-  '/assets/creativeartworks/artcret8.jpeg',
-  '/assets/creativeartworks/artcret9.jpeg',
+  '/artfolio/assets/creativeartworks/artcret7.jpeg',
+  '/artfolio/assets/creativeartworks/artcret8.jpeg',
+  '/artfolio/assets/creativeartworks/artcret9.jpeg',
   
   // Collection 4
-  '/assets/creativeartworks/artcret10.jpeg',
-  '/assets/creativeartworks/artcret11.jpeg',
-  '/assets/creativeartworks/artcret12.jpeg',
+  '/artfolio/assets/creativeartworks/artcret10.jpeg',
+  '/artfolio/assets/creativeartworks/artcret11.jpeg',
+  '/artfolio/assets/creativeartworks/artcret12.jpeg',
 
   // Collection 5
-  '/assets/creativeartworks/artcret13.jpeg',
-  '/assets/artreal/art9.jpeg',
-  '/assets/artreal/artgal1.jpeg',
+  '/artfolio/assets/creativeartworks/artcret13.jpeg',
+  '/artfolio/assets/artreal/art9.jpeg',
+  '/artfolio/assets/artreal/artgal1.jpeg',
 
 ];
 
 export const MURAL_IMAGES = [
   // Collection 1
-  '/assets/mural/mural1.jpeg',
-  '/assets/mural/mural3.jpeg',
-  '/assets/artreal/art3.jpeg',
+  '/artfolio/assets/mural/mural1.jpeg',
+  '/artfolio/assets/mural/mural3.jpeg',
+  '/artfolio/assets/artreal/art3.jpeg',
   
   
   // Collection 2
-  '/assets/mural/mural4.jpeg',
-  '/assets/mural/mural5.jpeg',
+  '/artfolio/assets/mural/mural4.jpeg',
+  '/artfolio/assets/mural/mural5.jpeg',
 ];
